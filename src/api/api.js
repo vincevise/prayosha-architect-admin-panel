61d9c7cbaeeafa7d6ea95f7765553c654c1fbe44
vincevise <vincentvuram204@gmail.com>
2023-01-29 00:03:37 +0530

    base url change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:8000/api'
+    baseURL:'https://ruby-snail-kit.cyclic.app/api'
 })
 
 export const createProject = async (data) => {
