b19ccbacd1b735c9693f7eb401725f0cc2914b8a
vincevise <vincentvuram204@gmail.com>
2023-01-29 16:36:18 +0530

    changes in config file url endpoint

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export const publicKey = "public_M9Jko+Xn7SVU3T6WKNxFvoc2iUk="
 export const urlEndpoint = "private_foW3ju0wy65HUb5hUoL0XDWS9hc="
-export const authenticationEndpoint = "https://ruby-snail-kit.cyclic.app/api/imagekit/auth"
+export const authenticationEndpoint = "https://prayosha-architect-backend.onrender.com/api/imagekit/auth"
